b293acd66c19ac60067d5fd9755cb73b4b570875
Arpita222 <arpita.22211327@viit.ac.in>
2026-01-18 16:17:42 +0530

    user and vedio model

--- src/app.js
+++ src/app.js
@@ -9,13 +9,15 @@ app.use(cors({
     credentials:true
 }))
 
-//json data store
+//json data store JSON body middleware 1)To read req.body 2)Protect server from very large payloads
 app.use(express.json({limit:"16kb"}))
 
-//url data store
+//url data store  URL encoded middleware Used when: Form data , URL encoded data
 app.use(express.urlencoded({extended:true, limit:"16kb"}))
 
+//Static files Used to: serve images,serve files
 app.use(express.static("public"))
 
+//To read cookies easily
 app.use(cookieParser())
 export {app}
